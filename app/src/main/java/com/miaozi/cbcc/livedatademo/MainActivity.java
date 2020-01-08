1f792c3dfd9c879eb6018c5ca508d9b87abee347
miaozi <miaozi@miaozideMBP.lan>
2020-01-08 10:30:17 +0800

    tijiao

---
+++ app/src/main/java/com/miaozi/cbcc/livedatademo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.miaozi.cbcc.livedatademo;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private MutableLiveData<MyLiveData> liveData = new MutableLiveData<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        liveData.observe(this, new Observer<MyLiveData>() {
+            @Override
+            public void onChanged(@Nullable MyLiveData myLiveData) {
+                Log.d("TAG","数据变化了: "+myLiveData.getId()+"");
+            }
+        });
+        MyLiveData myLiveData = new MyLiveData();
+        myLiveData.setId(1000);
+        liveData.setValue(myLiveData);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    MyLiveData myLiveData1 = new MyLiveData();
+                    myLiveData1.setId(30000);
+                    liveData.postValue(myLiveData1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+}
---
+++ app/src/main/java/com/miaozi/cbcc/livedatademo/MyLiveData.java
@@ -0,0 +1,18 @@
+package com.miaozi.cbcc.livedatademo;
+
+/*
+ * created by miaozi
+ * on 2020/1/7
+ */
+
+public class MyLiveData {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
